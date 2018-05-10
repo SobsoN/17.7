38f392f68b4cfb37634506479fce089b1e190641
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-05-10 18:40:02 +0200

    Fin

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '468435627949-iu0k86rd8tt2dl8bue3bb4aivt0cp9tu.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'LWJd5778MRyoJF4YVoRRVFp_',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
